e02ad736bc24a72232cc3e75ef0e7c9b970c16a4
PimtidaN455 <noey.caramel@gmail.com>
2022-07-30 05:19:16 -0700

    silde2

--- ProjectPhotoL-main/lib/page/PagesF/PageHomeAlbum/ImageSliderPage.dart
+++ ProjectPhotoL-main/lib/page/PagesF/PageHomeAlbum/ImageSliderPage.dart
@@ -4,8 +4,11 @@ import 'dart:math';
 import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:project_photo_learn/my_style.dart';
+import 'package:project_photo_learn/page/PagesF/PageHomeAlbum/ImagePage.dart';
 import 'package:project_photo_learn/page/PagesF/PageHomeAlbum/place.dart';
 import 'package:project_photo_learn/page/PagesF/PageHomeAlbum/places_data.dart';
+import 'package:project_photo_learn/page/PagesF/first.dart';
 
 class SlideImage extends StatelessWidget {
   @override
@@ -13,7 +16,35 @@ class SlideImage extends StatelessWidget {
     return MaterialApp(
       title: "",
       home: Scaffold(
-        appBar: AppBar(title: Text('odifhvoighv')),
+        appBar: AppBar(
+          title: Text("data"),
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+        ),
+        /* appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios_new,
+              color: MyStyle().blackColor,
+            ),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ShowImage()));
+            },
+          ),
+          backgroundColor: MyStyle().whiteColor,
+          title: Text(
+            "photo",
+            style: TextStyle(
+              fontSize: 30,
+              color: MyStyle().blackColor,
+              fontWeight: FontWeight.bold,
+              //fontStyle: FontStyle.normal,
+              fontFamily: 'Rajdhani',
+            ),
+          ),
+          automaticallyImplyLeading: false,
+        ),*/
         body: Body(),
       ),
     );
@@ -27,42 +58,97 @@ class Body extends StatefulWidget {
 
 class _Body extends State<Body> {
   //List<AllImage> imageListS = AllImages().getAllImages();
+  int currentIndex = 0;
+  final PageController controller = PageController();
+
   List<String> imagelist = [
-    "https://lzd-img-global.slatic.net/g/p/98843e93d9da2a6162f9a226fab34f36.jpg_2200x2200q80.jpg_.webp",
-    "https://cf.shopee.co.th/file/2ab0ec78bcf07e6c7972cbe5667100f7",
-    "https://images.hdqwalls.com/download/spiderman-peter-parker-standing-on-a-rooftop-ix-1280x720.jpg",
-    "https://images.wallpapersden.com/image/download/peter-parker-spider-man-homecoming_bGhsa22UmZqaraWkpJRmZ21lrWxnZQ.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvUgui-suS8DgaljlONNuhy4vPUsC_UKvxJQ&usqp=CAU",
+    "https://picsum.photos/id/240/200/300",
+    "https://picsum.photos/id/241/200/300",
+    "https://picsum.photos/id/242/200/300",
+    "https://picsum.photos/id/243/200/300",
+    "https://picsum.photos/id/244/200/300",
+    "https://picsum.photos/id/250/200/300",
+    "https://picsum.photos/id/251/200/300",
+    "https://picsum.photos/id/252/200/300",
+    "https://picsum.photos/id/253/200/300",
+    "https://picsum.photos/id/254/200/300",
   ];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: CarouselSlider(
-            options: CarouselOptions(
-              enlargeCenterPage: true,
-              enableInfiniteScroll: false,
-              autoPlay: true,
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: PageView.builder(
+              controller: controller,
+              onPageChanged: (index) {
+                setState(() {
+                  currentIndex = index % imagelist.length;
+                });
+              },
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: SizedBox(
+                    height: 300,
+                    width: double.infinity,
+                    child: Image.network(
+                      imagelist[index % imagelist.length],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              },
             ),
-            items: imagelist
-                .map((e) => ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Stack(
-                        fit: StackFit.expand,
-                        children: <Widget>[
-                          Image.network(
-                            e,
-                            width: 1050,
-                            height: 350,
-                            fit: BoxFit.cover,
-                          )
-                        ],
-                      ),
-                    ))
-                .toList(),
           ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (var i = 0; i < imagelist.length; i++)
+                buildIndicator(currentIndex == i)
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    controller.jumpToPage(currentIndex - 1);
+                  },
+                  icon: Icon(Icons.arrow_back),
+                ),
+                IconButton(
+                  onPressed: () {
+                    controller.jumpToPage(currentIndex + 1);
+                  },
+                  icon: Icon(Icons.arrow_forward),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildIndicator(bool isSelected) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 1),
+      child: Container(
+        height: isSelected ? 12 : 10,
+        width: isSelected ? 12 : 10,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: isSelected ? Colors.black : Colors.grey,
         ),
       ),
     );
